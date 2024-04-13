592bfd0a20e08e76dbf06e9747116e854a95d5f0
Yatharth Gupta <guptayatharth.yg@gmail.com>
2024-04-13 08:01:13 +0530

    added online check

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import resList from "../utils/MockData";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
-
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
   // Local State Variable - Super powerful variable
@@ -27,6 +27,14 @@ const Body = () => {
       json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
     );
   };
+  const onlineStatus = useOnlineStatus();
+
+  if (onlineStatus === false)
+    return (
+      <h1>
+        Looks like you're offline!! Please check your internet connection;
+      </h1>
+    );
   // console.log(resList)
   if (listOfRestaurants.length === 0 && searchText.length===0) {
     return <Shimmer />;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import { LOGO_URL } from "../utils/constants";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
   const [btnNameReact, setBtnNameReact] = useState("Login");
   //console.log("Header render");
-
+  const onlineStatus = useOnlineStatus();
   return (
     <div className="header">
       <div className="logo-container">
@@ -13,6 +14,7 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
+        <li>Online Status: {onlineStatus ? "✅" : "🔴"}</li>
           <li>
             <Link to="/">Home</Link>
           </li>
